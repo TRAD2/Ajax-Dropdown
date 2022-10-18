094408b59ab3b4909f4968f53a91f50e1c4c3b21
Yousef <amintrad1967@gmail.com>
2022-10-19 02:42:20 +0300

    adding data to the html table from the database, start codingajax

--- index.php
+++ index.php
@@ -31,10 +31,10 @@ include("config.php");
     <span> Fetch results by &nbsp; </span>
     <select name= "fetchval" id="fetchval">
         <option value="" disabled="" selected=""> Select Filter</option>
-        <option value="">Advertisement</option>
-        <option>Technology</option>
-        <option>Education</option>
-        <option>Fashion</option>
+        <option value="Advertisement">Advertisement</option>
+        <option value="Technology">Technology</option>
+        <option value="Education">Education</option>
+        <option value="Fashion">Fashion</option>
     </select>
 </div>
 
@@ -50,16 +50,45 @@ include("config.php");
             </tr>
         </thead>
         <tbody>
+            <?php 
+            $query = "SELECT * FROM posts";
+            $r = mysqli_query($conn,$query);
+            while($row = mysqli_fetch_assoc($r)){
+            ?>
             <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td><?php echo $row['p_no']?></td>
+                <td><?php echo $row['p_username']?></td>
+                <td><?php echo $row['p_tmg']?></td>
+                <td><?php echo $row['p_title']?></td>
+                <td><img src="img/<?php echo $row['p_img']?>" class="img-responsive img-thumbnail" width="150"></td>
             </tr>
+            <?php 
+            }
+            ?>
         </tbody>
     </table>
 </div>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $("#fetchval").on('change' ,function(){
+            var value = $(this).val();
+            //alert(value);
+
+            $.ajax({
+                url:"fetch.php",
+                type:"post",
+                data:"request=" + value;
+                beforesend:function(){
+                    $(".container").html("<h1>loading...</h1>");
+                },
+                success:function(){
+                    $(".container").html(data);
+                }
+            });
+        });
+    });
+</script>
 
 </body>
 </html>
