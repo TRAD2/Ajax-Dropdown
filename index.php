e12adf96c9c6d73a2ddf2eee861b0eac45b0ce75
Yousef <amintrad1967@gmail.com>
2022-10-18 19:49:25 +0300

    adding basic html\css page

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>ajax</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style type="text/css">
+    body{
+        margin:0;
+        padding:0;
+        font-family:;
+    }
+    #filters{
+        margin-left: 10%;
+        margin-top: 2%;
+        margin-bottom: 2%;
+    }
+
+  </style>
+</head>
+<body>
+
+<div id="filters">
+    <select name= "fetchval" id="fetchval">
+        <span> Fetch results by &nbsp; </span>
+        <option value="" disabled="" selected=""> Select Filter</option>
+        <option value="">Advertisement</option>
+        <option>Technology</option>
+        <option>Education</option>
+        <option>Fashion</option>
+    </select>
+</div>
+
+<div class="container">
+    <table class="table">
+        <thead>
+            <tr>
+                <th> Sr No. </th>
+                <th>Username</th>
+                <th>Date</th>
+                <th>Post Title</th>
+                <th>Post Image<th>
+            </tr>
+            I
+        </thead>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+</body>
+</html>
